82de18a82c3be7c5b0de2f0f2a3efffdec4ec6fa
HarshRRs <jainhatu@gmail.com>
2025-12-18 09:54:09 +0100

    Fix PrismaService: remove datasources from constructor

--- backend/src/prisma/prisma.service.ts
+++ backend/src/prisma/prisma.service.ts
@@ -6,13 +6,7 @@ export class PrismaService
   extends PrismaClient
   implements OnModuleInit, OnModuleDestroy {
   constructor() {
-    super({
-      datasources: {
-        db: {
-          url: process.env.DATABASE_URL,
-        },
-      },
-    });
+    super();
   }
 
   async onModuleInit() {
